c0ac4542d168173219cacd573c38008b13921439
Erick Oliveira <erickoliveirasystem@outlook.com>
2021-07-13 18:46:57 -0300

    Bugfix: vercel don't find the locales folder

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
+const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en-US",
     locales: ["en-US", "pt-BR"],
   },
+  localePath: path.resolve("./public/locales"),
 };
