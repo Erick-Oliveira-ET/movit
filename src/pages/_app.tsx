478c5477adf868d6d39da7796c37dc052deed36a
Erick Oliveira <erickoliveirasystem@outlook.com>
2021-08-31 20:14:24 -0300

    Google Analytics first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import { appWithTranslation } from "next-i18next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/global.css";
 
+import * as gtag from "../lib/ga";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", (url: URL) => {
+      gtag.pageView(url);
+    });
+    return () => {
+      router.events.off("routeChangeComplete", (url: URL) => {
+        gtag.pageView(url);
+      });
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
